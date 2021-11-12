048bbb9f7f3559beca91e13d3fa0f10f1956cafa
Nayeon Kim <nayeonk@lyft.com>
2021-11-12 01:19:11 -0800

    Add green scoring logic

--- draft_0.py
+++ draft_0.py
@@ -11,15 +11,17 @@ class Board:
     def __init__(self):
         self.pink_list = []
         self.blue_list = []
+        self.green_list = []
         #TODO: add others
     
-    # Equivalent to marking on the scoring sheet
+    # Equivalent to marking on the scoring sheet (die value for now)
     def add_to_list(self, die):
         if (get_color(die) == COLORS.pink):
-            self.pink_list.append(die)
+            self.pink_list.append(get_number(die))
         elif (get_color(die) == COLORS.blue):
             self.blue_list.append(die)
-            
+        elif (get_color(die) == COLORS.green):
+            self.green_list.append(die)
         
 class Player:
     def __init__(self, name: str):
@@ -40,14 +42,58 @@ class Player:
 
     def get_putback_count(self):
         return self.putback_count
-    
+
     # SCORING
-    def add_green_score(self, die):
-        raise NotImplemented
     def add_yellow_score(self, die):
         raise NotImplemented
     def add_black_score(self, die):
         raise NotImplemented
+    def add_green_score(self, die):
+        add_to_list(self.board, die) #"mark" on scoring sheet
+        green_index = len(self.board.green_list) #includes new input
+
+        if (green_index % 2) == 0: #if even numbered index, ready to score
+            current_die = self.board.green_list[green_index-1]
+            previous_die = self.board.green_list[green_index-2]
+            current_die_value = get_number(current_die)
+            previous_die_value = get_number(previous_die)
+
+            if (green_index==2):
+                self.reroll_count += 1
+                diff = (2*previous_die_value)-(2*current_die_value)
+                self.score += diff
+            elif (green_index==4):
+                blue_die_value = input("select blue die value between 1-6")
+                add_blue_score(self, Die(COLORS.blue,int(blue_die_value)))
+                diff = (2*previous_die_value)-(1*current_die_value)
+                self.score += diff
+            elif (green_index==6):
+                diff = (3*previous_die_value)-(3*current_die_value)
+                self.score += diff
+            elif (green_index==8):
+                black_die_value = input("select black die value between 1-6")
+                add_black_score(self, Die(COLORS.black,int(black_die_value)))
+                diff = (3*previous_die_value)-(2*current_die_value)
+                self.score += diff
+            elif (green_index==10):
+                diff = (3*previous_die_value)-(1*current_die_value)
+                self.score += diff
+            elif (green_index==12):
+                yellow_die_value = input("select yellow die value between 1-6")
+                add_yellow_score(self, Die(COLORS.yellow,int(yellow_die_value)))
+                diff = (4*previous_die_value)-(1*current_die_value)
+                self.score += diff
+        else:
+            if (green_index == 5):
+                self.putback_count += 1
+            elif (green_index == 7):
+                self.fox_count += 1
+            elif (green_index == 9):
+                self.plus_one_count += 1
+            elif (green_index == 11):
+                pink_die_value = input("select pink die value between 1-6")
+                add_pink_score(self, Die(COLORS.pink,int(pink_die_value)))
+
     def add_blue_score(self, die):
         add_to_list(self.board, die) #"mark" on scoring sheet
         blue_index = len(self.board.blue_list) #includes new input
@@ -57,14 +103,14 @@ class Player:
         elif (blue_index == 3):
             yellow_die_value = input("select yellow die value between 1-6")
             add_yellow_score(self, Die(COLORS.yellow,int(yellow_die_value)))
-        elif (blue_index == 5)
+        elif (blue_index == 5):
             self.plus_one_count += 1
-        elif (blue_index == 6)
+        elif (blue_index == 6):
             self.reroll_count += 1
-        elif (blue_index == 7)
+        elif (blue_index == 7):
             pink_die_value = input("select pink die value between 1-6")
             add_pink_score(self, Die(COLORS.pink,int(pink_die_value)))
-        elif (blue_index == 9)
+        elif (blue_index == 9):
             self.fox_count += 1
         elif (blue_index == 12):
             green_die_value = input("select green die value between 1-6")
